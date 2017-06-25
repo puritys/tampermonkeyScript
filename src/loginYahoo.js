3c94f6eb3c7deee5ae095ade93e68b6ec9439335
puritys <puritys.chen@gmail.com>
2017-06-25 22:53:40 +0800

    fix login

--- src/loginYahoo.js
+++ src/loginYahoo.js
@@ -13,20 +13,28 @@
     var passwords = {
        "account": "paswd"
     };
-    if (document.querySelector("#password-challenge")) {
-         var name = document.querySelector("#password-challenge .username").innerHTML;
-         name = name.replace(/Hello[^;]+;/, '');
-         if (passwords[name]) {
-            document.querySelector("#password-challenge input[type=password]").value = passwords[name];
-            document.querySelector("#password-challenge input[type=submit]").click();
 
-         }
-    }
+    var func = function () {
+        if (document.querySelector("#password-challenge")) {
+           var name = document.querySelector("#password-challenge .username").innerHTML;
+           name = name.replace(/Hello[^;]+;/, '');
+           if (passwords[name]) {
+              document.querySelector("#password-challenge input[type=password]").value = passwords[name];
+              document.querySelector("#password-challenge input[type=password]").blur();
+              setTimeout(func2, 1000);
+           }
+        }
     
-    // skip phone-obfuscation-challenge
-    if (document.querySelector("#phone-obfuscation-challenge")) {
-      //  document.querySelector("#phone-obfuscation-challenge input[type=submit]").click();
+        // skip phone-obfuscation-challenge
+        if (document.querySelector("#phone-obfuscation-challenge")) {
+        //  document.querySelector("#phone-obfuscation-challenge input[type=submit]").click();
 
-    }
- 
+        }
+    };
+    var func2 = function () {
+        document.querySelector("#login-signin").click();
+
+    };
+    setTimeout(func, 2000);
+
  })();
