65f4c2e05e94fcc255b668f1dfcc68a5cb451e75
Jackie Chen <pipi00000@gmail.com>
2018-02-09 14:08:52 +0800

    Update pixnet.js

--- src/pixnet.js
+++ src/pixnet.js
@@ -2,7 +2,7 @@
 // @name         pixnet copy/paste/menu
 // @namespace    https://github.com/puritys/tampermonkeyScript
 // @version      0.1
-// @description  回復痞客邦 pixnet 複製，貼上，右鍵選單等功能
+// @description  回復痞客邦 pixnet 複製，貼上，右鍵選單等功能，移除蓋版廣告
 // @author       puritys
 // @match        http://*.pixnet.net/*
 // @grant        none
